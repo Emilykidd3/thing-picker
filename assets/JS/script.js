2f776ebbb100adac5ef2130ad503298e51169545
emilykidd3 <e.kidd61@yahoo.com>
2021-08-25 21:12:10 -0700

    console logs options when pick button is chosen

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,18 @@
 var addThingButton = document.querySelector("#add-thing-button");
 var pickButton = document.querySelector("#pick-button");
 
+
+
 var addThing = function() {
-    console.log("add thing button clicked");
+    console.log("add thing clicked")
 }
 
-var pick = function() {
-    console.log("pick button clicked")
+var pick = function(event) {
+    event.preventDefault();
+    var optionOne = document.querySelector("#option-one").value;
+    var optionTwo = document.querySelector("#option-two").value;
+    console.log(optionOne);
+    console.log(optionTwo);
 }
 
 addThingButton.addEventListener("click", addThing);
