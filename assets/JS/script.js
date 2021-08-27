696da59c111487c560f0e1c3c13b6d15d236dc81
emilykidd3 <e.kidd61@yahoo.com>
2021-08-27 13:46:25 -0700

    response to pick will appear on page

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,7 +13,12 @@ var pick = function(event) {
     var optionTwo = document.querySelector("#option-two").value;
     var arr = [optionOne, optionTwo];
     var number = Math.floor(Math.random() * 2);
-    console.log(arr[number]);
+    var response = arr[number];
+    const elem = document.createElement('div');
+    const elemText = document.createTextNode(response);
+    elem.appendChild(elemText);
+    document.body.appendChild(elem);
+    elem.classList.add('response')
 }
 
 addThingButton.addEventListener("click", addThing);
